67eaf1a910b794bffaeaca3cd910c522688513d8
Matheus Henrique <matheusmhvs1@gmail.com>
2022-05-14 15:16:50 -0300

    :construction: Seção 3 em andamento

--- src/components/Section3.tsx
+++ src/components/Section3.tsx
@@ -1,6 +1,110 @@
 export function Section3(){
     return(
-        <h1>Hello Wolrd</h1>
+        <section 
+        style={{
+            background: '#F2F2F2',
+            border: '2px solid #0B7373',
+            borderRadius: '16px',
+            position: 'absolute',
+            width: '1412px',
+            height: '500px',
+            left: '254px',
+            top: '1176px'
+
+        }}
+        >
+            <h1
+                style={{
+                    position: 'absolute',
+                    width: '235px',
+                    height: '56px',
+                    left: '588px',
+                    top: '32px',
+
+                    fontFamily:'Roboto',
+                    fontStyle: 'normal',
+                    fontWeight: 700,
+                    fontSize:'32px',
+                    lineHeight: '56px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    textAlign: 'center',
+
+                    color: '#222222'
+                }}
+            >
+            Sobre Nós
+            </h1>
+            
+            <h3
+                style={{
+                    position: 'absolute',
+                    width: '78px',
+                    height: '28px',
+                    left: '258px',
+                    top: '161px',
+
+                    fontFamily:'Roboto',
+                    fontStyle: 'normal',
+                    fontWeight: 400,
+                    fontSize:'24px',
+                    lineHeight: '28px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    textAlign: 'center',
+                }}
+            >
+            Nome
+            </h3>
+
+            <p
+                style={{
+                    position: 'absolute',
+                    width: '916px',
+                    height: '129px',
+                    left: '258px',
+                    top: '200px',
+
+                    fontFamily:'Roboto',
+                    fontStyle: 'normal',
+                    fontWeight: 400,
+                    fontSize:'18px',
+                    lineHeight: '21px',
+                    display: 'flex',
+                    alignItems: 'center',
+
+                    color:'#000000'
+                }}
+            >
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi 
+            sit, eveniet alias, rem ut cumque iusto assumenda vitae eos asperiores 
+            porro est eius quas, odit cupiditate praesentium accusantium. Inventore, iste.
+            </p>
+
+            <button
+                style={{
+
+                }}
+            >
+            EMAIL
+            </button>
+
+            <button
+                style={{
+
+                }}
+            >
+            LINKEDIN
+            </button>
+            
+            <button
+                style={{
+
+                }}
+            >
+            GITHUB
+            </button>
+        </section>
     );
 }
 
